2d0711b57e9a40c491ca06d4679868c537b5b7d8
Saloni <95448361+Saloni-Salian@users.noreply.github.com>
2024-04-27 22:04:48 +0100

    Delete src/NER_model/save_tokenizer.py

--- src/NER_model/save_tokenizer.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoTokenizer
-
-model_checkpoint = "elastic/distilbert-base-uncased-finetuned-conll03-english"
-tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
-tokenizer.save_pretrained("ner/tokenizer")
